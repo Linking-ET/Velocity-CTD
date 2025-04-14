357e2c63a014c03a304fb8d75a3ccb679afa428d
R00tB33rMan <rootb33rman@gmail.com>
2025-04-14 00:42:11 -0400

    Revert "Revoke Configuration Phase changes due to persistent issues that are nearly impossible to resolve on various developers' fronts. Thanks for the feedback!"
    This reverts commit 7ab17ce3ac11843cf26b247bf7c52bcd77c40eaf.

--- proxy/src/main/java/com/velocitypowered/proxy/config/VelocityConfiguration.java
+++ proxy/src/main/java/com/velocitypowered/proxy/config/VelocityConfiguration.java
@@ -621,6 +621,10 @@ public final class VelocityConfiguration implements ProxyConfig {
     return advanced.isAllowIllegalCharactersInChat();
   }
 
+  public boolean isEnableConfigurationPhase() {
+    return advanced.isEnableConfigurationPhase();
+  }
+
   public String getServerBrand() {
     return advanced.getServerBrand();
   }
@@ -1301,6 +1305,8 @@ public final class VelocityConfiguration implements ProxyConfig {
     @Expose
     private boolean allowIllegalCharactersInChat = false;
     @Expose
+    private boolean enableConfigurationPhase = true;
+    @Expose
     private String serverBrand = "{backend-brand} ({proxy-brand})";
     private String serverBrandAsString;
     @Expose
@@ -1346,6 +1352,7 @@ public final class VelocityConfiguration implements ProxyConfig {
         this.maxPacketDataPerSecond = config.getIntOrElse("max-packet-data-per-second", 1 << 25);
         this.channelRegisterLimit = config.getIntOrElse("channel-register-limit", 1024);
         this.allowIllegalCharactersInChat = config.getOrElse("allow-illegal-characters-in-chat", false);
+        this.enableConfigurationPhase = config.getOrElse("enable-configuration-phase", true);
         this.serverBrand = config.getOrElse("server-brand", "{backend-brand} ({proxy-brand})");
         this.fallbackVersionPing = config.getOrElse("fallback-version-ping", "{proxy-brand} {protocol-min}-{protocol-max}");
         this.alwaysFallBackPing = config.getOrElse("always-fallback-ping", false);
@@ -1456,6 +1463,10 @@ public final class VelocityConfiguration implements ProxyConfig {
       return allowIllegalCharactersInChat;
     }
 
+    public boolean isEnableConfigurationPhase() {
+      return enableConfigurationPhase;
+    }
+
     public String getServerBrand() {
       return this.serverBrandAsString;
     }
@@ -1502,6 +1513,7 @@ public final class VelocityConfiguration implements ProxyConfig {
           + ", maxPacketDataPerSecond=" + maxPacketDataPerSecond
           + ", channelRegisterLimit=" + channelRegisterLimit
           + ", allowIllegalCharactersInChat=" + allowIllegalCharactersInChat
+          + ", enableConfigurationPhase=" + enableConfigurationPhase
           + '}';
     }
   }
